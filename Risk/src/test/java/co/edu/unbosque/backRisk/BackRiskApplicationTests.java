3ba96673d6a9bb43c325f875625f431d6d5bf8a9
mara-511 <maju514117@gmail.com>
2025-10-16 10:37:50 -0500

    creacion back

---
+++ Risk/src/test/java/co/edu/unbosque/backRisk/BackRiskApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.backRisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackRiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

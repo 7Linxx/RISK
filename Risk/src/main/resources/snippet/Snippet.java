134b5c0a200b45c73bdfefea19be4b8a90586235
mara-511 <maju514117@gmail.com>
2025-11-16 22:45:21 -0500

    war again

---
+++ Risk/src/main/resources/snippet/Snippet.java
@@ -0,0 +1,12 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		buf.append(current.getId()+",");
+						buf.append(current.getName()+",");
+						buf.append(current.getColor()+",");
+						buf.append(current.getNumberTroops()+",");
+						buf.append(current.getPlayerIndex()+";");
+	}
+}
+
